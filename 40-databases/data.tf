fff3488cbba377660fa356c2a3c23193aa568325
gantlapavan <gantlapavan1705@gmail.com>
2025-11-10 12:57:28 +0530

    some message

--- 40-databases/data.tf
+++ 40-databases/data.tf
@@ -35,6 +35,6 @@ data "aws_ssm_parameter" "mongodb_sg_id" {
 # }
 
 
-# data "aws_ssm_parameter" "database_subnet_ids" {
-#   name = "/${var.project_name}/${var.environment}/database_subnet_ids"
-# }
+data "aws_ssm_parameter" "database_subnet_ids" {
+  name = "/${var.project_name}/${var.environment}/database_subnet_ids"
+}
