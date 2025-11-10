17e5f45a6537b58dd4c863a0c15f088654a87ffa
gantlapavan <gantlapavan1705@gmail.com>
2025-11-10 12:47:06 +0530

    some message

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -1,5 +1,5 @@
 
-resource "aws_instance" "bastion" {
+resource "aws_instance" "mongodb" {
     ami = local.ami_id
     instance_type = "t3.micro"
     vpc_security_group_ids = [local.mongodb_sg_id]
