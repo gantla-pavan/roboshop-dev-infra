9b22b8fa497684f97336809b0b9b6453d34329f8
gantlapavan <gantlapavan1705@gmail.com>
2025-11-12 14:11:24 +0530

    some message

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -1,34 +1,23 @@
-  resource "aws_instance" "mongodb" {
+
+resource "aws_instance" "mongodb" {
     ami = local.ami_id
     instance_type = "t3.micro"
     vpc_security_group_ids = [local.mongodb_sg_id]
     subnet_id = local.database_subnet_id
-    # iam_instance_profile = aws_iam_instance_profile.bastion.name
-    # need more for terraform
-    # root_block_device {
-    #     volume_size = 50
-    #     volume_type = "gp3" # or "gp2", depending on your preference
-    # }
-
-    # user_data = file("bastion.sh")
     
     tags = merge (
         local.common_tags,
         {
-            Name = "${local.common_name_suffix}-mongodb"   #roboshop dev-mongodb
+            Name = "${local.common_name_suffix}-mongodb" # roboshop-dev-mongodb
         }
     )
+}
 
-    
-
-    }
-
-    resource "terraform_data" "mongodb" {
+resource "terraform_data" "mongodb" {
   triggers_replace = [
     aws_instance.mongodb.id
-    
   ]
-
+  
   connection {
     type     = "ssh"
     user     = "ec2-user"
@@ -36,10 +25,11 @@
     host     = aws_instance.mongodb.private_ip
   }
 
-   provisioner "file" {
-        source  =   "bootstrap.sh"
+  # terraform copies this file to mongodb server
+  provisioner "file" {
+    source = "bootstrap.sh"
     destination = "/tmp/bootstrap.sh"
-   }    
+  }
 
   provisioner "remote-exec" {
     inline = [
@@ -48,46 +38,27 @@
         "sudo sh /tmp/bootstrap.sh mongodb"
     ]
   }
- 
 }
 
-
-# resource "aws_iam_instance_profile" "bastion" {
-#   name = "bastion"
-#   role = "BastionTerraformAdmin"
-# }
-
-  resource "aws_instance" "redis" {
+resource "aws_instance" "redis" {
     ami = local.ami_id
     instance_type = "t3.micro"
     vpc_security_group_ids = [local.redis_sg_id]
     subnet_id = local.database_subnet_id
-    # iam_instance_profile = aws_iam_instance_profile.bastion.name
-    # need more for terraform
-    # root_block_device {
-    #     volume_size = 50
-    #     volume_type = "gp3" # or "gp2", depending on your preference
-    # }
-
-    # user_data = file("bastion.sh")
     
     tags = merge (
         local.common_tags,
         {
-            Name = "${local.common_name_suffix}-redis"   #roboshop dev-redis
+            Name = "${local.common_name_suffix}-redis" # roboshop-dev-redis
         }
     )
+}
 
-    
-
-    }
-
-    resource "terraform_data" "redis" {
+resource "terraform_data" "redis" {
   triggers_replace = [
     aws_instance.redis.id
-    
   ]
-
+  
   connection {
     type     = "ssh"
     user     = "ec2-user"
@@ -95,21 +66,19 @@
     host     = aws_instance.redis.private_ip
   }
 
-   provisioner "file" {
-        source  =   "bootstrap.sh"
+  # terraform copies this file to mongodb server
+  provisioner "file" {
+    source = "bootstrap.sh"
     destination = "/tmp/bootstrap.sh"
-   }    
+  }
 
   provisioner "remote-exec" {
     inline = [
         "chmod +x /tmp/bootstrap.sh",
-        # "sudo sh /tmp/bootstrap.sh"
         "sudo sh /tmp/bootstrap.sh redis"
     ]
   }
- 
 }
-
 
 
 resource "aws_instance" "rabbitmq" {
@@ -117,32 +86,20 @@ resource "aws_instance" "rabbitmq" {
     instance_type = "t3.micro"
     vpc_security_group_ids = [local.rabbitmq_sg_id]
     subnet_id = local.database_subnet_id
-    # iam_instance_profile = aws_iam_instance_profile.bastion.name
-    # need more for terraform
-    # root_block_device {
-    #     volume_size = 50
-    #     volume_type = "gp3" # or "gp2", depending on your preference
-    # }
-
-    # user_data = file("bastion.sh")
     
     tags = merge (
         local.common_tags,
         {
-            Name = "${local.common_name_suffix}-rabbitmq"   #roboshop dev-rabbitmq
+            Name = "${local.common_name_suffix}-rabbitmq" # roboshop-dev-rabbitmq
         }
     )
+}
 
-    
-
-    }
-
-    resource "terraform_data" "rabbitmq" {
+resource "terraform_data" "rabbitmq" {
   triggers_replace = [
     aws_instance.rabbitmq.id
-    
   ]
-
+  
   connection {
     type     = "ssh"
     user     = "ec2-user"
@@ -150,23 +107,98 @@ resource "aws_instance" "rabbitmq" {
     host     = aws_instance.rabbitmq.private_ip
   }
 
-   provisioner "file" {
-        source  =   "bootstrap.sh"
+  # terraform copies this file to mongodb server
+  provisioner "file" {
+    source = "bootstrap.sh"
     destination = "/tmp/bootstrap.sh"
-   }    
+  }
 
   provisioner "remote-exec" {
     inline = [
         "chmod +x /tmp/bootstrap.sh",
-        # "sudo sh /tmp/bootstrap.sh"
         "sudo sh /tmp/bootstrap.sh rabbitmq"
     ]
   }
- 
 }
 
+resource "aws_instance" "mysql" {
+    ami = local.ami_id
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [local.mysql_sg_id]
+    subnet_id = local.database_subnet_id
+    iam_instance_profile = aws_iam_instance_profile.mysql.name
+    
+    tags = merge (
+        local.common_tags,
+        {
+            Name = "${local.common_name_suffix}-mysql" # roboshop-dev-mysql
+        }
+    )
+}
 
-# resource "aws_iam_instance_profile" "bastion" {
-#   name = "bastion"
-#   role = "BastionTerraformAdmin"
+resource "aws_iam_instance_profile" "mysql" {
+  name = "mysql"
+  role = "EC2SSMParameterRead"
+}
+
+resource "terraform_data" "mysql" {
+  triggers_replace = [
+    aws_instance.mysql.id
+  ]
+  
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = aws_instance.mysql.private_ip
+  }
+
+  # terraform copies this file to mongodb server
+  provisioner "file" {
+    source = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+        "chmod +x /tmp/bootstrap.sh",
+        "sudo sh /tmp/bootstrap.sh mysql dev"
+    ]
+  }
+}
+
+# resource "aws_route53_record" "mongodb" {
+#   zone_id = var.zone_id
+#   name    = "mongodb-${var.environment}.${var.domain_name}" # mongodb-dev.daws86s.fun
+#   type    = "A"
+#   ttl     = 1
+#   records = [aws_instance.mongodb.private_ip]
+#   allow_overwrite = true
+# }
+
+# resource "aws_route53_record" "redis" {
+#   zone_id = var.zone_id
+#   name    = "redis-${var.environment}.${var.domain_name}" # redis-dev.daws86s.fun
+#   type    = "A"
+#   ttl     = 1
+#   records = [aws_instance.redis.private_ip]
+#   allow_overwrite = true
+# }
+
+# resource "aws_route53_record" "mysql" {
+#   zone_id = var.zone_id
+#   name    = "mysql-${var.environment}.${var.domain_name}" # mysql-dev.daws86s.fun
+#   type    = "A"
+#   ttl     = 1
+#   records = [aws_instance.mysql.private_ip]
+#   allow_overwrite = true
+# }
+
+# resource "aws_route53_record" "rabbitmq" {
+#   zone_id = var.zone_id
+#   name    = "rabbitmq-${var.environment}.${var.domain_name}" # rabbitmq-dev.daws86s.fun
+#   type    = "A"
+#   ttl     = 1
+#   records = [aws_instance.rabbitmq.private_ip]
+#   allow_overwrite = true
 # }
