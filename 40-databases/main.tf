ccfa97a7945f518781a0f77425ae692d5de35c9a
gantlapavan <gantlapavan1705@gmail.com>
2025-11-12 11:20:27 +0530

    some message

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -111,6 +111,61 @@
 }
 
 
+
+resource "aws_instance" "rabbitmq" {
+    ami = local.ami_id
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [local.rabbitmq_sg_id]
+    subnet_id = local.database_subnet_id
+    # iam_instance_profile = aws_iam_instance_profile.bastion.name
+    # need more for terraform
+    # root_block_device {
+    #     volume_size = 50
+    #     volume_type = "gp3" # or "gp2", depending on your preference
+    # }
+
+    # user_data = file("bastion.sh")
+    
+    tags = merge (
+        local.common_tags,
+        {
+            Name = "${local.common_name_suffix}-rabbitmq"   #roboshop dev-rabbitmq
+        }
+    )
+
+    
+
+    }
+
+    resource "terraform_data" "rabbitmq" {
+  triggers_replace = [
+    aws_instance.rabbitmq.id
+    
+  ]
+
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = aws_instance.rabbitmq.private_ip
+  }
+
+   provisioner "file" {
+        source  =   "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+   }    
+
+  provisioner "remote-exec" {
+    inline = [
+        "chmod +x /tmp/bootstrap.sh",
+        # "sudo sh /tmp/bootstrap.sh"
+        "sudo sh /tmp/bootstrap.sh rabbitmq"
+    ]
+  }
+ 
+}
+
+
 # resource "aws_iam_instance_profile" "bastion" {
 #   name = "bastion"
 #   role = "BastionTerraformAdmin"
