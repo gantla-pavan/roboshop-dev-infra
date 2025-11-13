313977794a0a07f13bf5603f9e058af5528b16a4
gantlapavan <gantlapavan1705@gmail.com>
2025-11-13 13:20:16 +0530

    some message

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -2,6 +2,8 @@ locals {
     common_name_suffix = "${var.project_name}-${var.environment}" #roboshop-dev
     private_subnet_id = split("," , data.aws_ssm_parameter.private_subnet_ids.value)[0]
     ami_id = data.aws_ami.joindevops.id
+      catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
+
     common_tags = {
         project = var.project_name
         environment = var.environment
